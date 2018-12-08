943c6408eebd8c54db4018856db494349b185975
J-KC-Man <justin_man@hotmail.co.uk>
2018-12-08 00:24:05 +0000

    commented code

--- app/src/main/java/com/jman/capstone_project/remoteDataSource/EndpointAsyncTask.java
+++ app/src/main/java/com/jman/capstone_project/remoteDataSource/EndpointAsyncTask.java
@@ -2,6 +2,9 @@ package com.jman.capstone_project.remoteDataSource;
 
 import android.os.AsyncTask;
 
+/**
+* Web client to interface with the API endpoint
+* */
 public class EndpointAsyncTask extends AsyncTask {
     @Override
     protected Object doInBackground(Object[] objects) {
