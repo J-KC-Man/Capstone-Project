6365e539d3507f1a2c84dd45fff5533d649d7e67
J-KC-Man <justin_man@hotmail.co.uk>
2018-12-08 00:18:39 +0000

    adds initial ui and sets up AsyncTask call

---
+++ app/src/main/java/com/jman/capstone_project/remoteDataSource/EndpointAsyncTask.java
@@ -0,0 +1,10 @@
+package com.jman.capstone_project.remoteDataSource;
+
+import android.os.AsyncTask;
+
+public class EndpointAsyncTask extends AsyncTask {
+    @Override
+    protected Object doInBackground(Object[] objects) {
+        return null;
+    }
+}
---
+++ app/src/main/java/com/jman/capstone_project/remoteDataSource/IAsyncTaskCallback.java
@@ -0,0 +1,12 @@
+package com.jman.capstone_project.remoteDataSource;
+
+/*
+ * This class acts as an interface between the AsyncTask and The UI.
+ * onResultReceived is called at onPostExecute to tell the callback object that the AsyncTask
+ * has finished and has received a result.
+ * This allows de-coupling of code between the AsyncTask and The UI.
+ * */
+public interface IAsyncTaskCallback {
+
+    void onResultReceived(String result);
+}
